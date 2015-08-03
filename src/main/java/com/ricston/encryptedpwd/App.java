c43429ef55cfd204d8aa16ae3172fa65a5ab6ac8
Justin Calleja <calleja.justin@gmail.com>
2015-08-03 15:30:27 +0200

    Removed unnecessary SpringBootServletInitializer (from copied code ^^;)

--- src/main/java/com/ricston/encryptedpwd/App.java
+++ src/main/java/com/ricston/encryptedpwd/App.java
@@ -7,12 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App {
 
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
